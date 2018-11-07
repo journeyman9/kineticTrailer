f2d65d6e94a7dc41aae82f585f069f6e910a776d
journeyman9 <journey.e.mcdowell@gmail.com>
2018-11-06 17:22:13 -0800

    Offset IC to begin at rear axle. Offset distance calc for goal for rear axle. Implemented jackknife check in simulink. Made +pi for going backwards to be more realizable

--- kinetic_trailer.m
+++ kinetic_trailer.m
@@ -19,7 +19,7 @@ a2 = 6.2941; %[m]
 L2 = 12.192; %[m]
 b2 = L2 - a2; %[m]
 C3 = 1057244; %[N/rad] 4 tires
-v = 4.5; %[m/s] 2.2352
+v = -4.5; %[m/s] 2.2352
 
 L1_star = a1 + h1; %[m]
 e1 = L1_star - L1; %[m]
@@ -109,23 +109,25 @@ F = MM(1:n, end-l+1:end);
 N = MM(end-m+1:end, end-l+1:end);
 
 %% Trajectory Generation
-track_vector = csvread('t_backward_straight.txt');
+track_vector = csvread('t_horizontal.txt');
 if v < 0
     fprintf('Going Backwards!')
-    track_vector(:, 4) = track_vector(:, 4) - pi;
+    track_vector(:, 4) = track_vector(:, 4) + pi;
 end
 
+hitch_max = 90; %[degrees]
+
 %% Simulink
-y_IC = 0.1;
+y_IC = 0;
 psi_2_IC = deg2rad(0) + track_vector(1, 4);
 hitch_IC = deg2rad(0);
 
 look_ahead = 0; %indices
 
 % x = [y_d1, psi_d1, theta_d, psi_1, psi_2, y2]
-psi_1_IC = hitch_IC + psi_2_IC;
+psi_1_IC = -hitch_IC + psi_2_IC;
 
-trailerIC = [track_vector(1, 1)-y_IC*sin(psi_2_IC), track_vector(1, 2)+y_IC*cos(psi_2_IC)]; %x2, y2
+trailerIC = [track_vector(1, 1)-y_IC*sin(psi_2_IC)+b2*cos(psi_2_IC), track_vector(1, 2)+y_IC*cos(psi_2_IC)+b2*sin(psi_2_IC)]; %x2, y2
 tractorIC = [trailerIC(1)+a2*cos(psi_2_IC)+h1*cos(psi_1_IC), trailerIC(2)+a2*sin(psi_2_IC)+h1*sin(psi_1_IC)]; %x1, y1
 ICs = [0; deg2rad(0); deg2rad(0); psi_1_IC; psi_2_IC; y_IC];
 
@@ -143,7 +145,6 @@ d1 = state(:, 6);
 
 %% Jack-knife check 
 hitch_angle = odometry(:, 8);
-hitch_max = 90; %[degrees]
 
 for terminal_index = 1:length(hitch_angle)
     if hitch_angle(terminal_index) > deg2rad(hitch_max)
